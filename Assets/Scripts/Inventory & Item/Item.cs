9ecfedde3ce4d72d2d3cf84964761ea3e73034f9
Jesper Ahlström <143940213+KebabMumsare@users.noreply.github.com>
2025-11-17 13:38:31 +0100

    Automation tweaks and weapon works with inv

--- Assets/Scripts/Inventory & Item/Item.cs
+++ Assets/Scripts/Inventory & Item/Item.cs
@@ -13,6 +13,7 @@ public class Item : ScriptableObject
     public ItemType type;
     public ActionType actionType;
     public Vector2Int range = new Vector2Int(5, 4);
+    public GameObject weaponPrefab;
 
     [Header("Only UI")]
     public bool stackable = true;
