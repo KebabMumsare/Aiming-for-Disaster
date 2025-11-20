f49ae24ca6ccebc0c714ee7045eea36f717d4b34
Eliah Dimmed <153503499+eliahdim@users.noreply.github.com>
2025-11-20 09:45:04 +0100

    UI and structure changes to XPbar and prefabs

--- Assets/Scripts/Player/PlayerXP.cs
+++ Assets/Scripts/Player/PlayerXP.cs
@@ -7,30 +7,37 @@ public class PlayerXP : MonoBehaviour
     public float currentXP = 0f;
     public float currentLevel = 1f;
     public float xpToNextLevel = 100f;
+    public int skillPoints = 1;
     public TextMeshProUGUI xpText;
     public TextMeshProUGUI levelText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        levelText.text = "LVL:" + currentLevel;
     }
 
     public void AddXP(float xp)
     {
         currentXP += xp;
         xpText.text = "XP: " + currentXP + " / " + xpToNextLevel;
+        CheckLevel();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        levelText.text = "LVL:" + currentLevel;
+    public void CheckLevel() {
         if (currentXP >= xpToNextLevel)
         {
             currentLevel++;
             currentXP = currentXP - xpToNextLevel;
             xpToNextLevel *= 1.1f;
             xpText.text = currentXP + " / " + xpToNextLevel;
+            skillPoints++;
         }
+        levelText.text = "LVL:" + currentLevel;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
     }
 }
