b535c997d02b50289c06a46340826e99b9088174
Carl Axelson <carl.axelson2006@gmail.com>
2025-12-18 11:54:36 +0100

    Added a video intro when start game button is clicked
    Added 'AimingForDisasterIntro.mp4' and associated meta files to the project. Updated StartMenu.cs to support playing the intro video using a VideoPlayer, triggering StartGame when the video finishes. Added a new render texture asset for video playback.

--- Assets/Scripts/UI/StartMenu.cs
+++ Assets/Scripts/UI/StartMenu.cs
@@ -1,8 +1,33 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Video;
 
 public class StartMenu : MonoBehaviour
 {
+    [SerializeField] public VideoPlayer videoPlayer;
+
+    private void OnEnable()
+    {
+        if (videoPlayer != null)
+        {
+            videoPlayer.loopPointReached += OnVideoFinished;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (videoPlayer != null)
+        {
+            videoPlayer.loopPointReached -= OnVideoFinished;
+        }
+    }
+
+    private void OnVideoFinished(VideoPlayer vp)
+    {
+        // When the video finishes, start the game
+        StartGame();
+    }
+
     public void StartGame()
     {
         // Loads the next scene in the build settings
